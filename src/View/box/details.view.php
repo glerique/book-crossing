b28c73f239da798a3b9ea1ba4ecc8a43ab7a24ab
glerique <gael.lerique@gmail.com>
2022-03-29 16:18:22 +0200

    Create new views

---
+++ src/View/box/details.view.php
@@ -0,0 +1,16 @@
+<div class="container">
+  <h2>Fiche autheur</h2>
+</div>
+<div class="container">
+  <form method="post" action="">
+
+    <div class="form-group mt-2">
+      <label for="lastName" class="form-label">Nom : </label>
+      <input type="text" class="form-control" name="lastName" value="<?= $result->getBoxName(); ?>" readonly>
+    </div>
+    <div class="form-group mt-2"><input type="button" class="btn btn-info" value="Retour" onClick="document.location.href = document.referrer" />
+    </div>
+  </form>
+
+
+</div>
